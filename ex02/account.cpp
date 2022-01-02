f90428a1c86db9174259e9356eb2fbe4d9f03d65
adidion19 <didionantoine@gmail.com>
2022-01-02 17:53:30 +0100

    ex02 avance

--- ex02/account.cpp
+++ ex02/account.cpp
@@ -6,7 +6,7 @@
 /*   By: adidion <adidion@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 14:51:09 by adidion           #+#    #+#             */
-/*   Updated: 2022/01/02 16:44:54 by adidion          ###   ########.fr       */
+/*   Updated: 2022/01/02 17:34:29 by adidion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 void Account::_displayTimestamp()
 {
@@ -44,30 +49,33 @@ void Account::_displayTimestamp()
 
 int	Account::getNbAccounts( void )
 {
-	return (Account::_nbAccounts);
+	return (_nbAccounts);
 }
 
 int	Account::getTotalAmount( void )
 {
-	return (Account::_totalAmount);
+	return (_totalAmount);
 }
 
 int	Account::getNbDeposits( void )
 {
-	return (Account::_totalNbDeposits);
+	return (_totalNbDeposits);
 }
 
 int	Account::getNbWithdrawals( void )
 {
-	return (Account::_totalNbWithdrawals);
+	return (_totalNbWithdrawals);
 }
 
-Account::Account( int initial_deposit )
+Account::Account( int initial_deposit ) : _amount(initial_deposit)
 {
-	Account::_displayTimestamp();
-	Account::_accountIndex = Account::_nbAccounts++;
-	std::cout << "index:" << Account::_accountIndex << ";amount";
-	
+	_displayTimestamp();
+	_accountIndex = _nbAccounts++;
+	std::cout << "index:" << _accountIndex << ";amount:"
+	<< _amount << ";created" << std::endl;
+	_totalAmount += _amount;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 	return ;
 }
 
@@ -76,6 +84,38 @@ Account::~Account( void )
 	;
 }
 
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:"
+	<< _totalAmount << ";deposits:" << getNbDeposits()
+	<< ";withdrawals:" << getNbWithdrawals() << std::endl;
+	return ;
+}
+
+void	Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:"
+	<< _amount << ";deposits:" << _nbDeposits
+	<< ";withdrawals:" << _nbWithdrawals << std::endl;
+	return ;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+	<< ";p_amount:" << _amount;
+	_amount += deposit;
+	_totalAmount += deposit;
+	_nbDeposits += 1;
+	_totalNbDeposits += 1;
+	std::cout << ";deposit:" << deposit
+	<< ";amount:" << _amount
+	<< ";nb_deposits:" << _nbDeposits << std::endl;
+	return ;
+}
 
 int main()
 {
@@ -86,6 +126,29 @@ int main()
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
 	accounts_t				accounts( amounts, amounts + amounts_size );
+	accounts_t::iterator	acc_begin	= accounts.begin();
+	accounts_t::iterator	acc_end		= accounts.end();
 
+	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
+	size_t const		d_size( sizeof(d) / sizeof(int) );
+	ints_t				deposits( d, d + d_size );
+	ints_t::iterator	dep_begin	= deposits.begin();
+	ints_t::iterator	dep_end		= deposits.end();
+
+	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
+	size_t const		w_size( sizeof(w) / sizeof(int) );
+	ints_t				withdrawals( w, w + w_size );
+	ints_t::iterator	wit_begin	= withdrawals.begin();
+	ints_t::iterator	wit_end		= withdrawals.end();
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+
+	for ( acc_int_t it( acc_begin, dep_begin );
+		  it.first != acc_end && it.second != dep_end;
+		  ++(it.first), ++(it.second) ) {
+
+		(*(it.first)).makeDeposit( *(it.second) );
+	}
+Account::displayAccountsInfos();
 	return (0);
 }
